5b15dded10a04be12f3fbd7c3b34af0dffe3a527
Dylon Thompson <dylont@bu.edu>
2024-04-10 18:06:17 -0400

    added potentially useful education data

--- scripts/World_bank_load_and_clean.R
+++ scripts/World_bank_load_and_clean.R
@@ -18,12 +18,37 @@ data_clean <- data_noNA|>
   pivot_wider(names_from = Indicator.Code, values_from = Proportion)
   
 
+### Education Data:
+educ_data <- data|>
+  filter(str_detect(Indicator.Code, '^SE.TER.CUAT.DO') | str_detect(Indicator.Code, '^SE.TER.CUAT.MS'))|>
+  select_if(~ !any(is.na(.))) |>
+  select(-c(1:3)) |>
+  rename_all(~gsub("X", "", .)) |>
+  group_by(Indicator.Code) |>
+  pivot_longer(cols = -c(1:2), names_to = 'Year', values_to = 'Proportion') |>
+  select(-1) |>
+  pivot_wider(names_from = Indicator.Code, values_from = Proportion)
+ 
 ### Indicator Reference data frame
-ref_table <- data_noNA|>
-  select(Indicator.Name, Indicator.Code)
+ref_table1 <- data_noNA|>
+  select(Indicator.Name, Indicator.Code) 
 
+ref_table2 <- data|>
+  filter(str_detect(Indicator.Code, '^SE.TER.CUAT.DO') | str_detect(Indicator.Code, '^SE.TER.CUAT.MS'))|>
+  select(Indicator.Name, Indicator.Code) 
+
+ref_table = rbind(ref_table1, ref_table2)
+  
 ### Save Data
 write_csv(data_clean, file = here::here('dataset', 'World_Bank_Employment_Data.csv'))
 saveRDS(data_clean, file = here::here('dataset/World_Bank_Employment_Data.RData'))
-  
+
+write_csv(educ_data, file = here::here('dataset', 'World_Bank_Educ_Data.csv'))
+saveRDS(educ_data, file = here::here('dataset/World_Bank_Educ_Data.RData'))
+
+write_csv(ref_table, file = here::here('dataset', 'World_Bank_Ref_Table.csv'))
+saveRDS(ref_table, file = here::here('dataset/World_Bank_Ref_Table.RData'))
+
+
+
 
