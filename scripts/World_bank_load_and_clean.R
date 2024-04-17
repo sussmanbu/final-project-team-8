37e61f984b1ce4fb5d20e020ec351234258d434a
Dylon Thompson <dylont@bu.edu>
2024-04-17 17:16:38 -0400

    working on heat maps

--- scripts/World_bank_load_and_clean.R
+++ scripts/World_bank_load_and_clean.R
@@ -12,10 +12,10 @@ data_noNA <- data[rowSums(is.na(data)) == 0, ]
 data_clean <- data_noNA|>
   select(-c(1:3)) |>
   rename_all(~gsub("X", "", .)) |>
-  group_by(Indicator.Code) |>
+  group_by(Indicator.Name) |>
   pivot_longer(cols = -c(1:2), names_to = 'Year', values_to = 'Proportion') |>
-  select(-1) |>
-  pivot_wider(names_from = Indicator.Code, values_from = Proportion)
+  select(-2) |>
+  pivot_wider(names_from = Indicator.Name, values_from = Proportion)
   
 
 ### Education Data:
@@ -24,21 +24,12 @@ educ_data <- data|>
   select_if(~ !any(is.na(.))) |>
   select(-c(1:3)) |>
   rename_all(~gsub("X", "", .)) |>
-  group_by(Indicator.Code) |>
+  group_by(Indicator.Name) |>
   pivot_longer(cols = -c(1:2), names_to = 'Year', values_to = 'Proportion') |>
-  select(-1) |>
-  pivot_wider(names_from = Indicator.Code, values_from = Proportion)
+  select(-2) |>
+  pivot_wider(names_from = Indicator.Name, values_from = Proportion)
  
-### Indicator Reference data frame
-ref_table1 <- data_noNA|>
-  select(Indicator.Name, Indicator.Code) 
 
-ref_table2 <- data|>
-  filter(str_detect(Indicator.Code, '^SE.TER.CUAT.DO') | str_detect(Indicator.Code, '^SE.TER.CUAT.MS'))|>
-  select(Indicator.Name, Indicator.Code) 
-
-ref_table = rbind(ref_table1, ref_table2)
-  
 ### Save Data
 write_csv(data_clean, file = here::here('dataset', 'World_Bank_Employment_Data.csv'))
 saveRDS(data_clean, file = here::here('dataset/World_Bank_Employment_Data.RData'))
@@ -46,8 +37,6 @@ saveRDS(data_clean, file = here::here('dataset/World_Bank_Employment_Data.RData'
 write_csv(educ_data, file = here::here('dataset', 'World_Bank_Educ_Data.csv'))
 saveRDS(educ_data, file = here::here('dataset/World_Bank_Educ_Data.RData'))
 
-write_csv(ref_table, file = here::here('dataset', 'World_Bank_Ref_Table.csv'))
-saveRDS(ref_table, file = here::here('dataset/World_Bank_Ref_Table.RData'))
 
 
 
